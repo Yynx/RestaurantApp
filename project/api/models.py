393166237ff304215052877fffab3c9cd6401829
lawcia <lawrencianjume@gmail.com>
2020-02-07 12:20:58 +0000

    create favourites model

--- project/api/models.py
+++ project/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Favourites(models.Model):
+    res_id = models.CharField(max_length=15)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
