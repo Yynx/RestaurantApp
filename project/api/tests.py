c065af9a606a5f96a80f84552093665c5ac3aa97
lawcia <lawrencianjume@gmail.com>
2020-02-09 15:13:18 +0000

    testing creating a new user

--- project/api/tests.py
+++ project/api/tests.py
@@ -1,3 +1,20 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 from django.test import TestCase
+import json
 
-# Create your tests here.
+# Test to see if we can register a new user
+class UserTests(APITestCase):
+    def test_create_new_user(self):
+        url = reverse('user-list')
+        data = {'username': 'moscowradish', 'email': 'radish@mail.com', 'password': 'radi77shy'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(json.loads(response.content), {'id': 1, 'username': 'moscowradish', 'email': 'radish@mail.com', 'password': 'radi77shy'})
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'moscowradish')
+        self.assertEqual(User.objects.get().email, 'radish@mail.com')
+        self.assertEqual(User.objects.get().password, 'radi77shy')
+
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from api.serializers import UserSerializer
+from rest_framework import generics
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
