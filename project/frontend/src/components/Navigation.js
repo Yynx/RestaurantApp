0ba9cec425c25e03f2cfed04c3dd6cd05b6a2339
Yynx <47715709+Yynx@users.noreply.github.com>
2020-02-10 10:31:07 +0000

    start navbar

--- project/frontend/src/components/Navigation.js
+++ project/frontend/src/components/Navigation.js
@@ -1,39 +1,96 @@
 import {Link} from "react-router-dom";
 import React, { Fragment } from 'react';
 import {Navbar} from 'react-bulma-components';
+import 
 
 const Navigation = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
     return (
-        <Navbar
-        color="white"
-        fixed="top"
-      >
-        <Navbar.Brand>
-          <Link className={"navbar-item"}to="/">
-            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
-          </Link>
-          <Navbar.Burger />
-        </Navbar.Brand>
-        <Navbar.Menu >
-          <Navbar.Container>
-           <Link className={"navbar-item"}to="/">
-               Home
-           </Link>
-           <Link className={"navbar-item"}to="/results">
-               Results
-           </Link>
-           <Link className={"navbar-item"}to="/profile">
-               Profile
-           </Link>
-           <Link className={"navbar-item"}to="/login">
-               Login
-           </Link>
-           <Link className={"navbar-item"}to="/signup">
-               Sign up
-           </Link>
-          </Navbar.Container>
-        </Navbar.Menu>
-      </Navbar>
+      <nav className="navbar" role="navigation" aria-label="main navigation" >
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <img src="/foogle.png" width="112" height="28" />
+        </Link>
+
+        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">
+            Home
+          </a>
+
+          <a className="navbar-item">
+            Documentation
+          </a>
+
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">
+              More
+            </a>
+
+            <div className="navbar-dropdown">
+              <a className="navbar-item">
+                About
+              </a>
+              <a className="navbar-item">
+                Jobs
+              </a>
+              <a className="navbar-item">
+                Contact
+              </a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
