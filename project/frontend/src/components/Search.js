eb228c4a5d0b849fe95ef65d0a0dacdbbf8b0b4d
lawcia <lawrencianjume@gmail.com>
2020-02-10 18:01:33 +0000

    add map

--- project/frontend/src/components/Search.js
+++ project/frontend/src/components/Search.js
@@ -20,24 +20,49 @@ class Search extends React.Component {
         } else if (this.props.searchKeyword) {
             url=`https://developers.zomato.com/api/v2.1/search?q=${this.props.searchKeyword}`
         }
+
+        let newYork = 'https://developers.zomato.com/api/v2.1/search?lat=40.7128&lon=74.0060'
      
         axios.get(url, {headers})
-        .then(response => this.setState({data : response}))
+        .then(response => this.setState({data : response}, () => this.leaflet()))
     }
+
+    leaflet = () => {
+       const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        const zoomLevel= 10;
+        const column =5;
+        const row = 2;
+        const accessToken = 'pk.eyJ1IjoibGF3Y2FrZSIsImEiOiJjazZnb3c3enUwOTg1M2pwOHJmcXNjdnNyIn0.2R2s_StXtwU8C8jDiQAXnA'
+        L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`, {
+         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: accessToken
+}).addTo(mymap);
+this.state.data.data.restaurants.map((restaurant) => { 
+    let marker = L.marker([restaurant.restaurant.location.latitude, restaurant.restaurant.location.longitude]).addTo(mymap)
+    marker.bindPopup(`<b>${restaurant.restaurant.name}</b><br/>${restaurant.restaurant.cuisines}<br/>Rating ${restaurant.restaurant.user_rating.aggregate_rating}`).openPopup()
+    })
+}
 
     componentDidMount() {
         this.getRestaurants()
+      
     }
     
     componentDidUpdate () {
-        console.log(this.state.data.data.restaurants)
+        console.log(this.state.data)
     }
 
     render() {
         return (
             <div>
                 <p>Results</p>
-                {this.state.data && this.state.data.data.restaurants.map((restaurant) => { return (<Restaurant res = {restaurant.restaurant} /> ) })}
+                <div id="mapid"></div>
+                
+                {this.state.data && this.state.data.data.restaurants.map((restaurant) => { 
+                    return (
+                <Restaurant res = {restaurant.restaurant} /> ) })}
             </div>
         )
     }
