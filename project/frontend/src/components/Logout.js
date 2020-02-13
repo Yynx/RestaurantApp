f4ba6c9c6023531ae0e74496230456034f5715fc
Yynx <47715709+Yynx@users.noreply.github.com>
2020-02-13 18:39:09 +0000

    logout page

--- project/frontend/src/components/Logout.js
+++ project/frontend/src/components/Logout.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 const Logout = (props) => {
-
+        
     return (
-        <div>
-            <p>Are you sure you want to log out?</p>
-            <button onClick={() => { 
-            localStorage.removeItem('token')
-            localStorage.removeItem('id')
-            props.logOut()}}>Yes</button>
+        <div class="logout-card">
+            <div class="card width-30">
+                <div class="card-content">
+                    <div class="content">
+                    Are you sure you want to log out?
+                    </div>
+                </div>
+
+                <footer class="card-footer">
+                    <button class="card-footer-item" onClick={() => { 
+                    localStorage.removeItem('token')
+                    props.logOut()}}>Yes</button>
+                </footer>
+            </div>
         </div>
     )
 }
