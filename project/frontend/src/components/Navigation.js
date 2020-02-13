7ba81e961ed0c11b53d8f92a27941411ed13a62e
lawcia <lawrencianjume@gmail.com>
2020-02-13 09:59:47 +0000

    burger isactive state added

--- project/frontend/src/components/Navigation.js
+++ project/frontend/src/components/Navigation.js
@@ -3,31 +3,7 @@ import React from 'react';
 import logo from '../images/foogle.png';
 
 const Navigation = (props) => {
-  document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
-    }
-  
-  });
+  const [isActive, setisActive] = React.useState(false);
 
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation" >
@@ -35,26 +11,29 @@ const Navigation = (props) => {
         <Link className="navbar-item" to="/">
           <img src={logo} alt="foogle-logo" width="auto" height="28" />
         </Link>
-
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" >
+        <a onClick={() => {
+              setisActive(!isActive);
+            }}
+            role="button"
+            className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample" >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div id="navbarBasicExample" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
         <div className="navbar-start">
           <Link className="navbar-item" to ="/">
             Find
           </Link>
-
           {props.loggedIn ?
           <Link className="navbar-item" to="/favourites">
          My Favourites
           </Link> : null}
           </div>
-
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
