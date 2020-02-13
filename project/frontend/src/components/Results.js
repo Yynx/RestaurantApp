3c9bfa4b8e892b574e21d56a76174147dee5f29b
lawcia <lawrencianjume@gmail.com>
2020-02-13 08:54:35 +0000

    sort by cuisine

--- project/frontend/src/components/Results.js
+++ project/frontend/src/components/Results.js
@@ -1,38 +1,46 @@
 import React from "react";
 import axios from "axios";
 import Restaurant from "./Restaurant";
-import mapIcon from "../images/pin.png";
+import Cuisines from "./Cuisines";
 
 class Results extends React.Component {
     
     state = {
         data : null,
         mymap : null,
-        layerGroup: null
+        layerGroup: null,
+        sort: '',
+        order: '',
+        cuisines: null,
+        cuisine: ''
     }
 
-    
     getRestaurants = () => {
         const headers = {
             'user-key': "89313d2549eb39affea00277f30d405d"
         }
-
         let url;
-
         if (this.props.latitude && this.props.longitude && this.props.searchKeyword) {
             url=`https://developers.zomato.com/api/v2.1/search?q=${this.props.searchKeyword}&lat=${this.props.latitude}&lon=${this.props.longitude}`
         } else if (this.props.latitude && this.props.longitude) {
             url=`https://developers.zomato.com/api/v2.1/search?lat=${this.props.latitude}&lon=${this.props.longitude}`
         }
-
-        // let newYork = 'https://developers.zomato.com/api/v2.1/search?lat=40.7128&lon=74.0060'
-     
         axios.get(url, {headers})
         .then(response => this.setState({data : response}, () => this.leaflet()))
     }
 
-    leaflet = () => {
+    getCuisines = () => {
+        const headers = {
+            'user-key': "89313d2549eb39affea00277f30d405d"
+        }
+        let url=`https://developers.zomato.com/api/v2.1/cuisines?&lat=${this.props.latitude}&lon=${this.props.longitude}`
+        axios.get(url, {headers})
+        .then(response => this.setState({cuisines : response.data.cuisines})
+        )
+           
+    }
 
+    leaflet = () => {
     let {layerGroup} = this.state
     let posMarker = L.marker([this.props.latitude, this.props.longitude]).addTo(layerGroup)
     posMarker.bindPopup(`<b>Finding places near here!</b>`).openPopup()
@@ -53,51 +61,82 @@ class Results extends React.Component {
             accessToken: accessToken
             }).addTo(mymap);
         let layerGroup = L.layerGroup().addTo(mymap)
-        
         this.setState({mymap: mymap, layerGroup: layerGroup})
     }
 
-    
     componentDidMount() {
         this.createMap()
         this.getRestaurants()
+        this.getCuisines()
         this.props.refreshState()
-      
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        if(prevState.order !== this.state.order || prevState.sort !== this.state.sort || prevState.cuisine !== this.state.cuisine){
+            this.filterResults()
+        }
     }
     
-    componentDidUpdate () {
-        //console.log(this.state.data)
+
+    filterResults = () => {
+    const headers = {
+    'user-key': "89313d2549eb39affea00277f30d405d"
+    }
+    let cuisineString = this.state.cuisine !== '' ?  `&cuisines=${this.state.cuisine}` : ''
+    let sortString = this.state.sort !== '' ?  `&sort=${this.state.sort}` : ''
+    let orderString = this.state.order !== '' ?  `&order=${this.state.order}` : ''
+    let url=`https://developers.zomato.com/api/v2.1/search?q=${this.props.searchKeyword}&lat=${this.props.latitude}&lon=${this.props.longitude}${cuisineString}${sortString}${orderString}`
+    axios
+    .get(url, {headers})
+    .then(response => {
+        let {layerGroup} = this.state
+        layerGroup.clearLayers()
+        this.setState({
+            data : response,
+            layerGroup: layerGroup}, () => this.leaflet())})
     }
 
-    sortBy = (event) => {
-        const headers = {
-            'user-key': "89313d2549eb39affea00277f30d405d"
-        }
-        let sortQuery = event.target.name;
-        let url;
-
-        if (this.props.latitude && this.props.longitude && this.props.searchKeyword) {
-            url=`https://developers.zomato.com/api/v2.1/search?q=${this.props.searchKeyword}&lat=${this.props.latitude}&lon=${this.props.longitude}&sort=${sortQuery}`
-        } else if (this.props.latitude && this.props.longitude) {
-            url=`https://developers.zomato.com/api/v2.1/search?lat=${this.props.latitude}&lon=${this.props.longitude}&sort=${sortQuery}`
-        }
-
-        axios.get(url, {headers})
-        .then(response => {
-            let {layerGroup} = this.state
-            layerGroup.clearLayers();
-            this.setState({data : response, 
-                          layerGroup: layerGroup
-                          }, () => this.leaflet()
-                          )
-            })
-
-        event.preventDefault();
-        //console.log(sortQuery)
+    handleOrderChange = (event) => {
+        const {name} = event.target
+        this.setState({
+          order: name
+        });
     }
 
+    handleSortChange = (event) => {
+        const {name} = event.target
+        this.setState({
+          sort: name
+        });
+    }
+
+
+    handleCuisineChange = (event) => {
+        const {name} = event.target
+        this.setState({
+          cuisine: name
+        });
+    }
+
+    filterByCuisine = () => {
+        let cuisinesObj = new Object()
+        let cusineResults = this.state.data ? this.state.data.data.restaurants.map((item) => {
+            let cuisinesStr = item.restaurant.cuisines
+            let cuisinesArray = cuisinesStr.split(',')
+            for(let i = 0; i < cuisinesArray.length; i++ ){
+                let cuisine = cuisinesArray[i].trim()
+               if(cuisinesObj.hasOwnProperty(cuisine)){
+                   cuisinesObj[`${cuisine}`] += 1
+               } else {
+                   cuisinesObj[`${cuisine}`] = 1
+               }
+            }
+            return cuisinesObj
+        }) : null
+        return cusineResults
+    }
     render() {
-
+       let cusineResults = this.filterByCuisine()
         return ( 
             <div>
                 <div id="mapid"></div>
@@ -107,21 +146,56 @@ class Results extends React.Component {
                         Sort by:
                     </p>
                     <ul class="menu-list">
-                        <li><a name="rating" onClick={(event) => this.sortBy(event)}>Rating</a></li>
-                        <li><a name="cost" onClick={(event) => this.sortBy(event)}>Cost</a></li>
-                        <li><a name="real_distance" onClick={(event) => this.sortBy(event)}>Distance</a></li>
+                    <li>
+                    <a
+                    name=""
+                    onClick={this.handleSortChange}
+                    ></a>
+                    </li>
+                    <li><a 
+                    name="cost"
+                    onClick={this.handleSortChange}
+                    >cost</a>
+                    </li>
+                    <li><a 
+                    name="rating"
+                    onClick={this.handleSortChange}                   
+                    >rating</a>
+                    </li>
+                    <li><a
+                    name="real_distance"
+                    onClick={this.handleSortChange}                    
+                    >distance</a></li>
                     </ul>
+                    <p class="menu-label">
+                        Order by:
+                    </p>
+                    <ul class="menu-list">
+                    <li><a
+                    name=""
+                    onClick={this.handleOrderChange}
+                    ></a></li>
+                    <li><a
+                    name="asc"
+                    onClick={this.handleOrderChange}
+                    >low to high</a></li>
+                    <li><a
+                    name="desc"
+                    onClick={this.handleOrderChange}
+                    >high to low</a></li>
+                    </ul>
+                    {this.state.cuisines && <p class="menu-label">Cuisine:</p>}
+                    {this.state.cuisines && cusineResults && <Cuisines 
+                    cuisines={this.state.cuisines} cusineResults={cusineResults}
+                    handleCuisineChange={this.handleCuisineChange} />}
                 </aside>
-
                 <div class="column">
-                
+                {this.props.searchKeyword && <h2>Showing results for {this.props.searchKeyword}</h2>}
                 {this.state.data && this.state.data.data.restaurants.map((restaurant) => { 
                     return (
                 <Restaurant res = {restaurant.restaurant} /> ) })}
                 </div>
-
                 </div>
-                
             </div>
         )
     }
