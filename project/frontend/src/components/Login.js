4be03368a58d293145bf1079c84de51cae46fc26
lawcia <lawrencianjume@gmail.com>
2020-02-14 10:24:15 +0000

    error messages for login

--- project/frontend/src/components/Login.js
+++ project/frontend/src/components/Login.js
@@ -5,7 +5,8 @@ class Login extends React.Component {
     state = {
         username: null,
         password: null,
-        email: null
+        email: null,
+        error: null
     }
 
     handleChange = (event) => {
@@ -17,12 +18,18 @@ class Login extends React.Component {
 
         axios.post('http://localhost:8000/api/v1/users/login', {username : this.state.username, password: this.state.password })
         .then((response) => {
-            //console.log(response.data)
+            
             localStorage.setItem('token', response.data.token)
-      
+           
             this.props.setUsername(response.data.username)
             
-            }).catch((error) => error)
+            this.setState({error: null})
+            }).catch((error) => {
+                let {non_field_errors} = error.response.data
+                if(non_field_errors){
+                   non_field_errors == "This username or email is not valid" ? this.setState({error: "This username is not valid"}) : this.setState({error: non_field_errors})
+                }
+            })
     }
 
     render() {
@@ -34,6 +41,7 @@ class Login extends React.Component {
                     <h3>Login</h3>
                     <input className="input input-m"onChange={this.handleChange} type="text" name="username" placeholder="Enter your username"></input>
                     <input className="input input-m" onChange={this.handleChange} type="password" name="password" placeholder="Enter your password"></input>
+                    {this.state.error && <p className="align-text error-msg">{this.state.error}</p>}
                     <button className="button is-success input-m">Log in</button>
                 </form>
             </div>
