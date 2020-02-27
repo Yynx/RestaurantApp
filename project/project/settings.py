a96cbff7579ebdedadccb959fc52cfa69c9a3073
Yynx <47715709+Yynx@users.noreply.github.com>
2020-02-27 15:31:52 +0000

    deploy to heroku

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 import psycopg2
 from dotenv import load_dotenv
 import datetime
@@ -91,6 +92,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
